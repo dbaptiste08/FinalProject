b1366584c9fc24ba960a7df7e47fcacfd2b89fa0
Sophia Scarangello <sophiascarangello@gmail.com>
2023-11-09 16:57:12 -0500

    :0

--- StudyAbroad/src/FacultyLead.java
+++ StudyAbroad/src/FacultyLead.java
@@ -1,4 +1,21 @@
-
+import java.util.HashSet;
 public class FacultyLead {
+	
+	private String name;
+	private String countryLocation;
+	private String subjectTaught;
+	private HashSet <String> nameList;
+	
+	public void Faculty(String name, String countryLocation, String subjectTaught) {
+		this.name = name;
+		this.countryLocation = countryLocation;
+		this.subjectTaught = subjectTaught;
+		this.nameList = new HashSet <>();
+		
+		
+	}
+	
+	
+	
 
 }
