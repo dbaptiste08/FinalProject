35c2f095f264c650ab0e8fbb8df1d36582d7483a
dbaptiste08 <149968815+dbaptiste08@users.noreply.github.com>
2023-11-09 16:23:21 -0500

    Starting point

--- StudyAbroad/src/Participant.java
+++ StudyAbroad/src/Participant.java
@@ -1,4 +1,13 @@
 
 public class Participant {
+	
+	protected String name;
+	protected int year;
+	protected String city;
+	protected String country;
+	
+	private static int participantCount = 0;
+	
+	//default constru
 
 }
