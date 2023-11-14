411a788286da27339e08f6d6f5702fee178b6401
dbaptiste08 <149968815+dbaptiste08@users.noreply.github.com>
2023-11-14 16:19:54 -0500

    delete city

--- StudyAbroad/src/Participant.java
+++ StudyAbroad/src/Participant.java
@@ -4,7 +4,6 @@ public class Participant {
 	protected String name;
 	protected int year;
 	protected int payment;
-	protected String city;
 	protected String country;
 	
 	private static int participantCount = 0;
@@ -13,24 +12,20 @@ public class Participant {
 	public Participant () {
 		name = "TBD";
 		year = 0;
-		city = "TBD";
 		country = "TBD";
 	}
 	
 	//intialize constructor
-	public Participant(String name, int y, String town, String ctry) {
+	public Participant(String name, String ctry) {
 		this.name = name;
-		year = y;
-		city = town;
 		country = ctry;
 	}
 	
 	//payment constructor
-	public Participant(String name, int y, int pay, String town, String ctry) {
+	public Participant(String name, int y, int pay, String ctry) {
 		this.name = name;
 		year = y;
 		payment = pay;
-		city = town;
 		country = ctry;
 		participantCount++;	
 	}
@@ -52,13 +47,6 @@ public class Participant {
 		this.year = year;
 	}
 
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
 
 	public String getCountry() {
 		return country;
@@ -80,7 +68,6 @@ public class Participant {
 		System.out.println("Thank you for your interest in study abroad!");
 		System.out.println("Name: " + name);
 		System.out.println("Year: " + year);
-		System.out.println("City: " + city);
 		System.out.println("Country: " + country);
 
 	}
