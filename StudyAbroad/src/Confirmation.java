ae8673812d52f3cb61bbfc0b8a4b8b4e7708e3a7
dbaptiste08 <149968815+dbaptiste08@users.noreply.github.com>
2023-11-15 12:11:53 -0500

    print correction

--- StudyAbroad/src/Confirmation.java
+++ StudyAbroad/src/Confirmation.java
@@ -29,21 +29,21 @@ public class Confirmation {
 		return payment;
 	}
 
-	public void setPayment(int payment) {
-	 if (payment < 500) {
+	public void setPayment(int payment) {	
 		this.payment = payment;
-	 }
-	 else {
-		 System.out.println("Error");
-	 }
-		
+	 		
 	}
 
 	
 	public void printFinal() {
-		System.out.println("You officially have a spot for the study aborad program!");
-		System.out.println("We are so excited to have you!");
-	}
-		
+        System.out.println("********************************************************");
+        System.out.println("You officially have a spot for the study abroad program!");
+        if (payment < 500) {
+            System.out.println("Error: Payment must be at least 500 for confirmation. You are not signed up for the study abroad program.");
+        } else {
+            System.out.println("You have officially signed up! We are so excited to have you!");
+        }
+        System.out.println("********************************************************");
+	}		
 
 }
