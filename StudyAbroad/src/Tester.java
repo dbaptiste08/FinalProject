95301b3219ace59d777d7ff3a968d2133a7a5103
dbaptiste08 <149968815+dbaptiste08@users.noreply.github.com>
2023-11-14 21:37:10 -0500

    more scenarios

--- StudyAbroad/src/Tester.java
+++ StudyAbroad/src/Tester.java
@@ -4,7 +4,21 @@ public class Tester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Test");
+		Participant Kelly = new Participant("Kelly", "France", 2, "Maymester");
+		Kelly.printInfo();
+		
+		Confirmation KellyPayment = new Confirmation("Kelly", 500);
+		KellyPayment.printFinal();
+		
+		StudyAbroad info = new StudyAbroad();
+		info.printCountries();
+		info.printLength();
+		
+		FacultyLead faculty1 = new FacultyLead();
+		faculty1.Faculty("Claire Fraiser", "Italy", "History");
+		faculty1.addToNameList("Lucy");
+		faculty1.addToNameList("Jamie");
+		faculty1.print();
 		
 		FacultyLead faculty = new FacultyLead();
 		faculty.Faculty("Karen Aguar", "Italy", "MIS");
