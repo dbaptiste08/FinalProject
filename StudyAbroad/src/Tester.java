049ae207f224fa368e0ecc3bbadf023e64e4a0cc
dbaptiste08 <149968815+dbaptiste08@users.noreply.github.com>
2023-11-15 12:12:38 -0500

    print correction

--- StudyAbroad/src/Tester.java
+++ StudyAbroad/src/Tester.java
@@ -4,6 +4,9 @@ public class Tester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		System.out.println("Welcome to Study Abroad!");
+		System.out.println("Please write your name, country, year, and semester");
+		
 		Participant Kelly = new Participant("Kelly", "France", 2, "Maymester");
 		Kelly.printInfo();
 		
