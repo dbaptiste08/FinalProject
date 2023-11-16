f20348cbdf640a7726717f87eca80cfe6e31bbab
dbaptiste08 <149968815+dbaptiste08@users.noreply.github.com>
2023-11-16 13:34:41 -0500

    edits

--- StudyAbroad/src/Tester.java
+++ StudyAbroad/src/Tester.java
@@ -4,30 +4,47 @@ public class Tester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Welcome to Study Abroad!");
-		System.out.println("Please write your name, country, year, and semester");
-		
-		Participant Kelly = new Participant("Kelly", "France", 2, "Maymester");
-		Kelly.printInfo();
-		
-		Confirmation KellyPayment = new Confirmation("Kelly", 500);
-		KellyPayment.printFinal();
-		
-		
+		StudyAbroad welcome = new StudyAbroad();
+		welcome.printWelcomeStatement();
 		
 		StudyAbroad info = new StudyAbroad();
 		info.printCountries();
 		info.printLength();
 		
+		
+		StudyAbroad prompt = new StudyAbroad();
+		prompt.printPrompt();
+		
+		//First example
+		Participant Kelly = new Participant("Kelly", "France", 248, "Maymester");
+		Kelly.printInfo();
+		
+		Confirmation KellyPayment = new Confirmation("Kelly", 500, 248);
+		KellyPayment.printFinal();
+		
+		//second example
+		Participant Sophia = new Participant("Sophia", "Italy", 799, "Spring");
+		Sophia.printInfo();
+		
+		Confirmation SophiaPayment = new Confirmation("Sophia", 500, 799);
+		SophiaPayment.printFinal();
+		
+		//error example
+		Participant Danielle = new Participant("Danielle", "Italy", 345, "Spring");
+	    Danielle.printInfo();
+	    
+	    Confirmation DaniellePayment = new Confirmation("Danielle", 300, 345);
+		DaniellePayment.printFinal();
+	    				
+		
+		
 		FacultyLead faculty1 = new FacultyLead();
-		faculty1.Faculty("Claire Fraiser", "Italy", "History");
-		faculty1.addToNameList("Lucy");
-		faculty1.addToNameList("Jamie");
+		faculty1.Faculty("Claire Fraiser", "France", "History");
+		faculty1.addToNameList("Kelly");
 		faculty1.print();
 		
 		FacultyLead faculty = new FacultyLead();
 		faculty.Faculty("Karen Aguar", "Italy", "MIS");
-		faculty.addToNameList("Danielle");
 		faculty.addToNameList("Sophia");
 		faculty.print();
 		
